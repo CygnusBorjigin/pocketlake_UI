a27f0a639bd6de50ee98ffd7637a5f53a7250db6
SomeoneSomewhere <cygnusborjigin@gmail.com>
2022-10-15 18:31:21 -0400

    finish webpage setup

---
+++ src/components/GuiSample.jsx
@@ -0,0 +1,9 @@
+const GuiSample = () => {
+    return(
+        <div className={"h-screen w-screen bg-gray-200"}>
+            <h1>ThrorMap Graphical User Interface Demo</h1>
+        </div>
+    )
+}
+
+export default GuiSample;
